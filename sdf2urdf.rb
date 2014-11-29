5e6d6519f8f369f7f86773e47c9ca72260986add
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2014-11-29 16:33:36 +0000

    most of <link> and <joint> are now converted

--- sdf2urdf.rb
+++ sdf2urdf.rb
@@ -2,38 +2,107 @@
 
 require 'nokogiri'
 
-include Nokogiri
+def convert_pose(parent, builder)
+  sdf_pose = parent.>("pose").first
+  return if sdf_pose.nil?
 
-## TODO: At the end, check if this is still needed
-class Object
-  def map(&block)
-    block.call(self) unless nil?
-  end
-end
-
-class Nokogiri::XML::Node
-  def child_named(name)
-    existing = children.find {|c| c.name == name }
-    return existing if existing
-    
-    child = XML::Element.new(name, self.document)
-    self << child
-    return child
-  end
-end
-
-def assert(cond, err_msg)
-  throw Exception.new(err_msg) if not cond
-end
-
-def transform_pose(elm, options={})
-  tokens = elm.text.split
-  origin = element('origin')
-  if not options[:compact_origin] or not tokens.all? {|x| x.to_f == 0 }
+  origin = builder.origin
+  tokens = sdf_pose.text.split
+  if not tokens.all? {|x| x.to_f == 0 }
     origin['xyz'] = tokens[0,3].join(" ")
     origin['rpy'] = tokens[3,3].join(" ")
   end
-  origin
+end
+
+def convert_geometry(parent, builder)
+  parent.>("geometry").each do |sdf_geom|
+    builder.geometry {
+      sdf_geom.element_children.each do |sdf_prim|
+        attrs = {}
+        sdf_prim.element_children.each do |param|
+          attrs[param.name] = param.text
+        end
+        
+        builder.send(sdf_prim.name, attrs)
+      end
+    }
+  end
+end
+
+def convert_links(parent, out)
+  parent.>('link').each do |sdf_link|
+    out.link(:name => sdf_link[:name]) {
+
+      sdf_link.xpath('/visual').each do |sdf_visual|
+        out.visual {
+          convert_pose(sdf_visual, out)
+          convert_geometry(sdf_visual, out)
+        }
+      end
+
+      if sdf_inertial = sdf_link.>("inertial").first
+        out.inertial {
+          convert_pose(sdf_inertial, out)
+          
+          if sdf_mass = sdf_inertial.>("mass").first
+            out.mass(:value => sdf_mass.text)
+          end
+          
+          if sdf_inertia = sdf_inertial.>("inertia").first
+            attrs = {}
+            sdf_inertia.element_children.each do |item|
+              attrs[item.name] = item.text # if item.name =~ /^i[xyz][xyz]$/
+            end
+            out.inertia(attrs)
+          end
+        }
+      end
+
+      sdf_link.>("collision").each do |sdf_collision|
+        out.collision {
+          convert_pose(sdf_collision, out)
+          convert_geometry(sdf_collision, out)
+        }
+      end # do |sdf_visual|
+    } # out.link
+  end # do |sdf_link|
+end
+
+def flatten_tag(builder, in_parent, out_tag, bindings)
+  attrs = {}
+  bindings.each do |xpath, out_attr|
+    if child = in_parent.at_xpath(xpath) 
+      attrs[out_attr] = child.text
+    end
+  end
+  
+  builder.method_missing(out_tag, attrs) unless attrs == {}
+end
+
+URDF_VALID_JOINTS = "revolute continuous prismatic fixed floating planar".split
+def convert_joints(parent, out)
+  parent.>("joint").each do |sdf_joint|
+    if not URDF_VALID_JOINTS.include? sdf_joint[:type]
+      warn "URDF does not support joint type `#{sdf_joint[:type]}'"
+    else
+      out.joint(:name => sdf_joint[:name], :type => sdf_joint[:type]) {
+        convert_pose(sdf_joint, out)
+
+        flatten_tag(out, sdf_joint, "child", "child" => :link)
+        flatten_tag(out, sdf_joint, "parent", "parent" => :link)
+        flatten_tag(out, sdf_joint, "axis", "xyz" => :xyz)
+        flatten_tag(out, sdf_joint, "dynamic", 
+                    "axis/dynamic/damping" => :damping,
+                    "axis/dynamic/friction" => :friction)
+
+        flatten_tag(out, sdf_joint, "limit",
+                    "axis/limit/lower"    => :lower,
+                    "axis/limit/upper"    => :upper,
+                    "axis/limit/effort"   => :effort,
+                    "axis/limit/velocity" => :velocity)
+      }  
+    end
+  end
 end
 
 ## Input
@@ -41,70 +110,18 @@ input_f = ARGV.length > 0 ? File.open(ARGV[0]) : STDIN
 input = Nokogiri::XML(input_f)
 abort("root element is not <sdf>, but <#{input.root.name}>") unless input.root.name == "sdf"
 
-model = input.root.>('model').first
+model = input.at_xpath('/sdf/model')
 abort("No <model> tag") if model.nil?
 
+STDERR.puts "Robot name: `#{model[:name]}'"
+
 ## Output
-@outdoc = XML::Document.new
+builder = Nokogiri::XML::Builder.new do |out|
+  out.robot(:name => model[:name]) {
+    convert_links(model, out)
+    convert_joints(model, out)
+  } # out.robot
+end # do |out|
 
-def element(name, attrs={}, children=[])
-  elm = XML::Element.new(name, @outdoc)
-  attrs.each {|k,v| elm[k] = v }
-  children.each {|n| elm << n }
-  elm
-end
-
-## Translation
-robot = element('robot', { 'name' => model['name'] })
-
-base_link = element('link', { 'name' => 'base' })
-robot << base_link
-
-model.>('pose').each do |src_pose|
-  origin = transform_pose(src_pose)
-  base_link.child_named("visual") << origin
-  base_link.child_named("collision") << origin.dup
-end
-
-model.>('link').each do |src_link|
-  link = element('link', { 'name' => src_link['name'] })
-  
-  src_link.>('pose').each do |src_pose|
-    origin = transform_pose(src_pose)
-    link.child_named("visual") << origin
-    link.child_named("collision") << origin.dup
-  end
-  
-  src_link.>('inertial').first.map do |src_inertial|
-    inertial = element('inertial')
-    src_inertial.>("pose").first.map do |src_pose|
-      inertial << transform_pose(src_pose, {:compact_origin => true})
-    end
-    
-    src_inertial.>("mass").first.map do |src_mass|
-      inertial << element('mass', { 'value' => src_mass.text })
-    end
-    
-    src_inertial.>("inertia").first.map do |src_inertia|
-      inertia = element('inertia')
-      src_inertia.children.each do |item|
-        inertia[item.name] = item.text # if item.name =~ /^i[xyz][xyz]$/
-      end
-      inertial << inertia
-    end
-    
-    link << inertial
-  end
-  
-  src_link.>('collision').first.map do |src_collision|
-  end
-  
-  src_link.>('visual').first.map do |src_visual|
-  end
-  
-  robot << link
-end
-
-@outdoc.root = robot
-puts @outdoc.to_s
+puts builder.to_xml
 
